4d919c741bc2e9917859b0a2c88ea2af32afcb2f
patrick andrews <patrickxandrews@gmail.com>
2023-04-19 12:07:44 -0600

    build out functions

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,6 +1,10 @@
 var functions = require("firebase-functions");
 var admin = require('firebase-admin');
 var serviceAccount = require("./firebase-credentials.json");
+require('dotenv').config();
+var openai = require("openai")
+
+const ai = new openai(process.env.OPENAI_API_KEY);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -21,56 +25,104 @@ exports.createEspressoRecord = functions.https.onRequest(async (request, respons
 
 // reads a specific espresso record
 exports.readEspressoRecord = functions.https.onRequest(async (request, response) => {
-    const res = { body: "" }
+    const id = request.query.id;
+    if (!id) {
+        response.status(400).send("Espresso record id is required");
+        return;
+    }
+    const res = await admin.firestore().collection('espresso').doc(id).get();
     response.set('Access-Control-Allow-Origin', '*');
-    response.status(200).send(JSON.stringify(res.body));
+    response.status(200).send(JSON.stringify(res));
 });
 
 // reads all espresso records
 exports.readEspressoRecords = functions.https.onRequest(async (request, response) => {
-    const res = { body: "" }
+    const res = await admin.firestore().collection('espresso').get();
     response.set('Access-Control-Allow-Origin', '*');
-    response.status(200).send(JSON.stringify(res.body));
+    response.status(200).send(JSON.stringify(res));
 });
 
 // updates a specific espresso record
 exports.updateEspressoRecord = functions.https.onRequest(async (request, response) => {
-    const res = { body: "" }
+    const id = request.query.id;
+    if (!id) {
+        response.status(400).send("Espresso record id is required");
+        return;
+    }
+    const espresso = request.body.espresso;
+    if (!espresso) {
+        response.status(400).send("Espresso record is required");
+        return;
+    }
+    const res = await admin.firestore().collection('espresso').doc(id).update(espresso);
     response.set('Access-Control-Allow-Origin', '*');
-    response.status(200).send(JSON.stringify(res.body));
+    response.status(200).send(JSON.stringify("Espresso record updated"));
 });
 
 // deletes a specific espresso record
 exports.deleteEspressoRecord = functions.https.onRequest(async (request, response) => {
-    const res = { body: "" }
+    const id = request.query.id;
+    if (!id) {
+        response.status(400).send("Espresso record id is required");
+        return;
+    }
+    const res = await admin.firestore().collection('espresso').doc(id).delete();
     response.set('Access-Control-Allow-Origin', '*');
     response.status(200).send(JSON.stringify(res.body));
 });
 
 // fetches an espresso suggestion from chat ai, based on the provided query
 exports.fetchChatAiEspressoSuggestion = functions.https.onRequest(async (request, response) => {
-    const res = { body: "" }
+    const query = request.body.query;
+    if (!query) {
+        response.status(400).send("Query is required");
+        return;
+    }
+    const res = await ai.complete({
+        engine: "davinci",
+        prompt: query,
+        maxTokens: 5,
+        temperature: 0.9,
+        topP: 1,
+        presencePenalty: 0,
+        frequencyPenalty: 0,
+        bestOf: 1,
+        n: 1,
+        stream: false,
+        stop: ["\n", " User:", " AI:"]
+    });
     response.set('Access-Control-Allow-Origin', '*');
-    response.status(200).send(JSON.stringify(res.body));
+    response.status(200).send(JSON.stringify(res.data));
 });
 
 // logs in a user with email and password
 exports.emailPasswordLogin = functions.https.onRequest(async (request, response) => {
-    const res = { body: "" }
+    const email = request.body.email;
+    const pass = request.body.pass;
+    if (!email || !pass) {
+        response.status(400).send("Email and password are required");
+        return;
+    }
+    await admin.auth().signInWithEmailAndPassword(email, pass);
     response.set('Access-Control-Allow-Origin', '*');
-    response.status(200).send(JSON.stringify(res.body));
-});
-
-// logs in a user with google
-exports.googleLogin = functions.https.onRequest(async (request, response) => {
-    const res = { body: "" }
-    response.set('Access-Control-Allow-Origin', '*');
-    response.status(200).send(JSON.stringify(res.body));
+    response.status(200).send(JSON.stringify("Successfully logged in"));
 });
 
 // logs out a user
 exports.logout = functions.https.onRequest(async (request, response) => {
-    const res = { body: "" }
+    await admin.auth().signOut();
     response.set('Access-Control-Allow-Origin', '*');
-    response.status(200).send(JSON.stringify(res.body));
+    response.status(200).send(JSON.stringify("Successfully logged out"));
+});
+
+// sends a password reset email to the provided email address
+exports.resetPassword = functions.https.onRequest(async (request, response) => {
+    const email = request.body.email;
+    if (!email) {
+        response.status(400).send("Email is required");
+        return;
+    }
+    await admin.auth().sendPasswordResetEmail(email);
+    response.set('Access-Control-Allow-Origin', '*');
+    response.status(200).send(JSON.stringify("Password reset email sent"));
 });
