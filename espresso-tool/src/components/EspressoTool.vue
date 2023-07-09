621da26d8087b6d5ec3cfbf0a7c630b4f2254b58
patrick andrews <patrickxandrews@gmail.com>
2023-07-09 15:51:05 -0600

    more boiler / build out api call

--- espresso-tool/src/components/EspressoTool.vue
+++ espresso-tool/src/components/EspressoTool.vue
@@ -1,97 +1,66 @@
 <template>
-  <div class="espresso">
-    <div class="user-input-left">
-      <h2>Coffee Info</h2>
-      <label>Country</label>
-      <input type="text" placeholder="Country" v-model="coffeeInfo.country" />
-      <label>Region</label>
-      <input type="text" placeholder="Region" v-model="coffeeInfo.region" />
-      <label>Processing</label>
-      <input
-        type="text"
-        placeholder="Processing"
-        v-model="coffeeInfo.processing"
-      />
-      <label>Altitude</label>
-      <input type="text" placeholder="Altitude" v-model="coffeeInfo.altitude" />
-      <div v-for="note in coffeeInfo.listedNotes" :key="note.id">
-        <label>Tasting Note {{ note.id + 1 }}</label>
-        <input type="text" placeholder="Tasting Note" v-model="note.note" />
+  <div>
+    <div class="espresso">
+      <div class="user-input-right">
+        <h2>Coffee Info</h2>
+        <label>Country</label>
+        <input type="text" v-model="coffeeInfo.country" />
+        <label>Region</label>
+        <input type="text" v-model="coffeeInfo.region" />
+        <label>Processing</label>
+        <input type="text" v-model="coffeeInfo.processing" />
+        <label>Altitude</label>
+        <input type="text" v-model="coffeeInfo.altitude" />
+        <label>Roast Level</label>
+        <input type="text" v-model="coffeeInfo.roast" />
+        <div v-for="note in coffeeInfo.tastingNotes" :key="note.id">
+          <label>Tasting Note {{ note.id + 1 }}</label>
+          <input type="text" v-model="note.note" />
+        </div>
+      </div>
+      <!-- <div class="user-input-right">
+        <h2>Current Recipe</h2>
+        <label>Dose</label>
+        <input type="text" v-model="currentRecipe.dose" />
+        <label>Yield</label>
+        <input type="text" v-model="currentRecipe.yield" />
+        <label>Time</label>
+        <input type="text" v-model="currentRecipe.time" />
+        <div v-for="note in currentRecipe.tastingNotes" :key="note.id">
+          <label>Getting Note {{ note.id + 1 }}</label>
+          <input type="text" v-model="note.note" />
+        </div>
+      </div> -->
+      <div class="suggestion-left">
+        <h2>Suggested Recipe</h2>
+        <label>Dose</label>
+        <input disabled="true" type="text" v-model="suggestedRecipe.dose" />
+        <label>Yield</label>
+        <input disabled="true" type="text" v-model="suggestedRecipe.yield" />
+        <label>Time</label>
+        <input disabled="true" type="text" v-model="suggestedRecipe.time" />
+        <label>Notes</label>
+        <textarea
+          disabled="true"
+          v-model="suggestedRecipe.note"
+          rows="15"
+          style="resize: none"
+        ></textarea>
       </div>
     </div>
-    <div class="user-input-right">
-      <h2>Current Recipe</h2>
-      <label>Dose</label>
-      <input type="text" placeholder="Dose" v-model="currentRecipe.dose" />
-      <label>Yield</label>
-      <input type="text" placeholder="Yield" v-model="currentRecipe.yield" />
-      <label>Time</label>
-      <input type="text" placeholder="Time" v-model="currentRecipe.time" />
-      <label>Grind</label>
-      <input type="text" placeholder="Grind" v-model="currentRecipe.grind" />
-      <label>Temperature</label>
-      <input
-        type="text"
-        placeholder="Temperature"
-        v-model="currentRecipe.temperature"
-      />
-      <div v-for="note in currentRecipe.gettingNotes" :key="note.id">
-        <label>Getting Note {{ note.id + 1 }}</label>
-        <input type="text" placeholder="Tasting Note" v-model="note.note" />
-      </div>
-    </div>
-    <div class="suggestion-left">
-      <h2>Suggested Recipe</h2>
-      <label>Dose</label>
-      <input
-        disabled="true"
-        type="text"
-        placeholder="Dose"
-        v-model="suggestedRecipe.dose"
-      />
-      <label>Yield</label>
-      <input
-        disabled="true"
-        type="text"
-        placeholder="Yield"
-        v-model="suggestedRecipe.yield"
-      />
-      <label>Time</label>
-      <input
-        disabled="true"
-        type="text"
-        placeholder="Time"
-        v-model="suggestedRecipe.time"
-      />
-      <label>Grind</label>
-      <input
-        disabled="true"
-        type="text"
-        placeholder="Grind"
-        v-model="suggestedRecipe.grind"
-      />
-      <label>Temperature</label>
-      <input
-        disabled="true"
-        type="text"
-        placeholder="Temperature"
-        v-model="suggestedRecipe.temperature"
-      />
-    </div>
-    <div class="suggestion-right">
-      <h2>-</h2>
-      <label>Notes</label>
-      <textarea
-        disabled="true"
-        placeholder="Notes"
-        v-model="suggestedRecipe.notes"
-        rows="15"
-      ></textarea>
+    <div class="button-bar">
+      <button :disabled="!submitValid || loading" @click="getSuggestion()">Get Suggestion</button>
+      <p class="error">{{  error  }}</p>
     </div>
   </div>
 </template>
 
 <script>
+const { Configuration, OpenAIApi } = require("openai");
+const aiConfig = new Configuration({
+    apiKey: process.env.VUE_APP_OPENAI_API_KEY,
+});
+const ai = new OpenAIApi(aiConfig);
 export default {
   name: "EspressoTool",
   props: {},
@@ -102,7 +71,8 @@ export default {
         region: "",
         processing: "",
         altitude: "",
-        listedNotes: [
+        roast: "",
+        tastingNotes: [
           { id: 0, note: "" },
           { id: 1, note: "" },
           { id: 2, note: "" },
@@ -112,9 +82,7 @@ export default {
         dose: 0,
         yield: 0,
         time: 0,
-        grind: 0,
-        temperature: 0,
-        gettingNotes: [
+        tastingNotes: [
           { id: 0, note: "" },
           { id: 1, note: "" },
           { id: 2, note: "" },
@@ -124,26 +92,59 @@ export default {
         dose: 0,
         yield: 0,
         time: 0,
-        grind: 0,
-        temperature: 0,
-        notes: "",
+        note: "",
       },
-      attempts: 0,
+      loading: false,
+      error: ''
     };
   },
-  methods: {},
+  methods: {
+    async getSuggestion() {
+      this.error = '';
+      this.loading = true;
+      const query = `I'm trying to dial in a ${this.coffeeInfo.processing} ${this.coffeeInfo.country} espresso. It was grown at ${this.coffeeInfo.altitude} altitude. It's a ${this.coffeeInfo.roast} roast level. The tasting notes listed on the bag are ${this.coffeeInfo.tastingNotes}. Can you suggest a recipe to me, in the following JSON format: { "yield" : '10g', "dose": '10g', "time": '10s', "note": 'here are some helpful tips to dial in easier' }. Please only return the JSON part, no additional text. Will you try to get a unique recipe based on the parameters of the coffee, instead of always saying 1:2 ratio in 30 seconds?`
+      const res = await ai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: query }],
+      });
+      try {
+      this.suggestedRecipe = JSON.parse(res.data.choices[0].message.content);
+      } catch (e) {
+        console.log(e)
+        this.error = 'Something went wrong, please try again.'
+      }
+      this.loading = false;
+    },
+  },
+  computed: {
+    submitValid() {
+      return this.coffeeInfo.country.length > 1 && this.coffeeInfo.region.length > 1 && this.coffeeInfo.processing.length > 1 && this.coffeeInfo.altitude.length > 1 && this.coffeeInfo.roast.length > 1 && this.coffeeInfo.tastingNotes[0].note.length > 1 && this.coffeeInfo.tastingNotes[1].note.length > 1 && this.coffeeInfo.tastingNotes[2].note.length > 1
+    }
+  },
 };
 </script>
 
 <style scoped>
+@font-face {
+  font-family: BNMainz;
+  src: url("../assets/BNMainz.woff");
+}
+
+@font-face {
+  font-family: Hanover-Bold;
+  src: url("../assets/Hanover-Bold.woff");
+}
+
 @media screen and (max-width: 1100px) {
-  .espresso {
+  .espresso,
+  .button-bar {
     width: 90%;
   }
 }
 
 @media screen and (min-width: 1100px) {
-  .espresso {
+  .espresso,
+  .button-bar {
     width: 50%;
   }
 }
@@ -151,7 +152,7 @@ export default {
 .espresso {
   background-color: rgb(10, 0, 10);
   display: grid;
-  grid-template-columns: repeat(12, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
   border: 1px solid rgb(208, 255, 0);
   margin: 0 auto;
   border-radius: 15px;
@@ -160,42 +161,117 @@ export default {
   margin-top: 20px;
 }
 
-input, textarea {
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: Hanover-Bold;
+}
+
+h2 {
+  font-size: 1em;
+  margin-bottom: 10px;
+}
+
+button {
+  background-color: rgb(208, 255, 0);
+  color: rgb(10, 0, 10);
+  border: 1px solid rgb(208, 255, 0);
+  padding: 5px;
+  width: 150px;
+  font-size: 1em;
+  font-family: BNMainz;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 0 auto;
+}
+
+button:hover {
+  background-color: rgb(10, 0, 10);
+  color: rgb(208, 255, 0);
+}
+
+button:hover:disabled {
+  background-color: rgb(208, 255, 0);
+  color: rgb(10, 0, 10);
+  cursor: not-allowed;
+}
+
+button:disabled,
+button[disabled] {
+  opacity: 0.5;
+}
+
+input:disabled,
+input[disabled],
+textarea:disabled,
+textarea[disabled] {
+  border: 0px;
+}
+
+input,
+textarea {
   background-color: rgb(10, 0, 10);
   color: rgb(208, 255, 0);
   border: 1px solid rgb(208, 255, 0);
-  border-radius: 5px;
-  padding: 1px;
+  padding: 5px;
   margin: 5px;
   width: 90%;
   font-size: 1em;
-  font-family: sans-serif;
+  font-family: BNMainz;
+}
+
+input::placeholder,
+textarea::placeholder {
+  color: rgb(208, 255, 0);
+  font-family: BNMainz;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
 }
 
 label {
   font-size: 1em;
-  font-family: sans-serif;
+  font-family: BNMainz;
   margin-left: 5px;
 }
 
+.button-bar {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 10px;
+}
+
 .user-input-right {
-  grid-column: 4 / 7;
   padding: 15px;
   border-right: 1px solid rgb(208, 255, 0);
 }
 
 .user-input-left {
-  grid-column: 1 / 4;
   padding: 15px;
 }
 
 .suggestion-left {
-  grid-column: 7 / 10;
   padding: 15px;
 }
 
 .suggestion-right {
-  grid-column: 10 / 13;
   padding: 15px;
+}
+
+.error {
+  color: rgb(255, 0, 106);
+  font-family: BNMainz;
+  font-size: 1em;
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 10px;
 }
 </style>
